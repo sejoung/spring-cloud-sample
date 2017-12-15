67667ca2ced79c1b44967fc780d3475cc109e444
sejoung <sejoung@gmail.com>
2017-12-15 12:16:51 +0900

    eureka 샘플코드

---
+++ spring-eureka-server/spring-eureka-server/src/main/java/com/github/sejoung/spring/cloud/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.github.sejoung.spring.cloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kim se joung
+ *
+ */
+@SpringBootApplication
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
