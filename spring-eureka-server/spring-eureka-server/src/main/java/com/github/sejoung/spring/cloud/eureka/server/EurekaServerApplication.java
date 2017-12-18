2228eeab1c10b564f6ece4ac3495d4a5614b747b
sejoung <sejoung@gmail.com>
2017-12-18 11:07:02 +0900

    소스수정

--- spring-eureka-server/spring-eureka-server/src/main/java/com/github/sejoung/spring/cloud/eureka/server/EurekaServerApplication.java
+++ spring-eureka-server/spring-eureka-server/src/main/java/com/github/sejoung/spring/cloud/eureka/server/EurekaServerApplication.java
@@ -2,11 +2,13 @@ package com.github.sejoung.spring.cloud.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author kim se joung
  *
  */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 	public static void main(String[] args) {
